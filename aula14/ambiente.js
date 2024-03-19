39d88e5110feb553e7c378af1c8bdb60ec2c66fd
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-03-19 18:26:11 -0300

    repetições - parte2

---
+++ aula14/ambiente.js
@@ -0,0 +1,29 @@
+// ESTRUTURAS DE REPETIÇÃO
+console.log(`Vai começar...`)
+
+for (var c = 0; c < 10 ; c++) {
+    console.log(c)
+}
+console.log(`Fim!`)
+
+
+/* FOR
+for (var c = 0; c < 10 ; c++) {
+    console.log(c)
+}
+*/
+
+/* WHILE
+var c = 0
+do {
+    console.log(c)
+    c++
+}while(c < 10)
+
+// -------------
+
+while(c < 10) {
+    console.log(c)
+    c++
+}
+*/
