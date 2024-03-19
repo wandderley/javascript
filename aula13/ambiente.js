43a86977ac3ab1d1ee94ba5ebb522e516176e974
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-03-19 18:04:43 -0300

    repetições - parte1

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+// ESTRUTURAS DE REPETIÇÃO
+var i = 0
+
+do {
+    console.log(`${i} - Tudo bem?`)
+    i++
+} while(i < 10)
+
+/*
+while (i < 10){
+    console.log(`${i} - Tudo bem?`)
+    i++
+}
+*/
+
+/*
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
