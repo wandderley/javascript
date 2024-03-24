7af16e89bbc68b080faf668f44dcf767126662b0
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-03-23 22:37:34 -0300

    aula17 - introdução a objeto

---
+++ aula17/objeto01.js
@@ -0,0 +1,10 @@
+var amigo = {nome: 'José',
+sexo: 'M',
+peso: 85.4,
+engorgar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engorgar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
