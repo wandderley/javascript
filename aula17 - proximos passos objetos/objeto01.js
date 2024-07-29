f82c61339447fbba5c5c6b471e1782d9ac96aec3
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-07-29 18:23:39 -0300

    títulos das pastas alterados

--- aula17 - proximos passos objetos/objeto01.js
+++ aula17 - proximos passos objetos/objeto01.js
@@ -1,10 +1,10 @@
 var amigo = {nome: 'José',
 sexo: 'M',
 peso: 85.4,
-engorgar(p=0){
+engordar(p=0){
     console.log('Engordou')
     this.peso += p
 }}
 
-amigo.engorgar(2)
+amigo.engordar(2)
 console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
